9ef95e47a1fc921ca30605f6adb13db4a84581db
mayevskyivan5@gmail.com <mayevskyivan5@gmail.com>
2021-02-09 17:07:37 +0200

    add mycontext

--- ZNO_History/Answer.cs
+++ ZNO_History/Answer.cs
@@ -1,10 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace ZNO_History
 {
-    class Answer
+    public class Answer
     {
+        [Table("tblAnswers")]
+        public class AnswerAudit
+        {
+            [Key]
+            public int Id { get; set; }
+
+            [Required, StringLength(250)]
+            public string Text { get; set; }
+
+            public bool IsTrue { get; set; }
+
+            [ForeignKey("Question")]
+            public int QuestionId { get; set; }
+            public virtual Question Question { get; set; }
+        }
     }
 }
--- ZNO_History/MyContext.cs
+++ ZNO_History/MyContext.cs
@@ -1,10 +1,16 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace ZNO_History
 {
-    class MyContext
+   public class MyContext : DbContext
     {
+        DbSet<Answer> Answers { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Server=95.214.10.36;Port=5432;Database=mayevskydb;Username=maey;Password=$544$B77w**G)K$t!Ube22}mav");
+        }
     }
 }
